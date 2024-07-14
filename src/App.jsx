3b7992d3837848b6ad9e805f230536648f0c8c1a
Shehan Jayasooriya <116498102+ShehanJay00@users.noreply.github.com>
2024-07-14 16:37:52 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 
 const App = () => {
@@ -12,11 +13,12 @@ const App = () => {
         {color:'red'}
         }>Hello World
       </h1>
-
+      <Route>
       <Header/>
       <Hero/>
       <Contact/>
       <Footer/>
+      </Route>
     </div>
   );
 };
